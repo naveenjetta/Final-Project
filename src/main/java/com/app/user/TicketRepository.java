1b18b09ac4371f39957b8006cc729f49ce0e0a71
naveenjetta <160380084+naveenjetta@users.noreply.github.com>
2024-07-13 10:06:24 +0530

    Delete src directory

--- src/main/java/com/app/user/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.user.Ticket;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
